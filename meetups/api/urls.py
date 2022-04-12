79da4199d445d1b79b8eaeca71d97666d5a751b3
sifatnabil <sifatnabil@gmail.com>
2022-04-12 15:59:52 +0600

    Fixed the login fields in Swagger

--- meetups/api/urls.py
+++ meetups/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
--- meetups/api/views.py
+++ meetups/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.
 from rest_framework.response import Response
 from meetups.models import Meetup
 from .serializers import MeetupSerializer, RegistrationSerializer
